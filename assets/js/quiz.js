8abc0eeee24bd69869bc18df4588f30721868d98
Kadrilaanes <kadrilaanes1@hotmail.com>
2020-11-24 09:07:25 +0000

    Game.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,11 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
-let questionsCounter = 0;
-let availableQuestions = [];
+let questionCounter = 0;
+let availableQuesions = [];
 
 let questions = [
     {
@@ -188,4 +188,46 @@ let questions = [
         choice2: "Zhangjiajie, Huaihua, China",
         answer: 2,
     },
-]
+];
+
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 30;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame();
