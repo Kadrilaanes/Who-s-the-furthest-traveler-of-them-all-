f51fe848db9f487217b934910b2386594ef9ba25
Kadrilaanes <kadrilaanes1@hotmail.com>
2020-11-28 21:36:33 +0000

    Created displayResult function for the end.html

---
+++ assets/js/end.js
@@ -0,0 +1,22 @@
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const finalScore = document.getElementById("finalScore");
+finalScore.innerText = mostRecentScore;
+
+
+let result1 = "Nice try! There's definitely space for improvement though. Take a look at the Library for new vacation destinations and discover the world.";
+let result2 = "Well done! You have recognized a lot of our landmarks! Take a look at the Library to add somew new destination ideas to your travel-list.";
+let result3 = "You did great! You know our landmarks well. If this was too easy for you, do not hesitate to leave me recommendations for unusual but recognizeable landmarks in the Contact form.";
+
+function displayResult(mostRecentScore) {
+    var parsed = parseInt(mostRecentScore);
+    if (parsed <= 10) {
+        return result1;
+    } else if (parsed > 10 || parsed <= 20) {
+        return result2;
+    } else {
+        return result3;
+    };
+};
+
+const result = document.getElementById("result");
+result.innerText = displayResult(); 
